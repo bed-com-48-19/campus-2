2b1df428a0786a12e65de5a26dcd1e966d5df6bc
unknown <bed-com-48-19@unima.ac.mw>
2023-01-06 12:21:44 +0200

    Little change in responsiveness

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   ];
   let [open,setOpen]=useState(false);
 return (
-  <div className='bg-indigo-800'>
+  <div className='bg-indigo-100 '>
     <div className='md:flex items-center justify-between bg-gray-800 py-2 md:px-10 px-7'>
     <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
     text-gray-800'>
@@ -20,11 +20,6 @@ return (
 	  <div className="Deco"><p className='text-white font-bold '> Campus Mgt System </p>
 	  </div>
     </div>
-    
-    <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-    <ion-icon name={open ? 'close':'menu'}></ion-icon>
-    </div>
-
     <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-gray-800 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
       {
         Links.map((link)=>(
